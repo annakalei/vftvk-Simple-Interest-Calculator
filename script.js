b3078781cfbcfac8d2faacc745d729109d17cc2b
annakalei <104076752+annakalei@users.noreply.github.com>
2022-04-20 22:06:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function compute() {
    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>"at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 }
 //update
-function updateRate(onchange) 
+function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
